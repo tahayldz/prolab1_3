96e5ddde733f95d84d20277d4db8599b9c96606d
tahayldz <44941791+tahayldz@users.noreply.github.com>
2019-12-14 18:08:18 +0300

    Update asd.c

--- asd.c
+++ asd.c
@@ -6,7 +6,7 @@ typedef struct liste
 {
     int plaka;
     char isim[30];
-    char bolge[2];
+    char bolge[3];
     int komsuSayisi;
     struct liste *next;
     struct liste *prev;
@@ -32,9 +32,7 @@ liste *listeOku(liste *head)
     }
     else
     {
-
-
-        while (!feof (dosya))
+      while (!feof (dosya))
         {
 
             char *satirOku, *satirTut;
@@ -43,9 +41,6 @@ liste *listeOku(liste *head)
             if (head == NULL)
             {
                 head = malloc(sizeof(liste));
-
-
-
                 satirOku=fgets(satir,90,dosya);
 
                 for(i=0; i<strlen(satir); i++)
@@ -55,79 +50,50 @@ liste *listeOku(liste *head)
                         virgul++;
                     }
                 }
-
                 yaz=fopen("OUTPUT.txt","w+");
-
-                 satirTut = strtok(satir,",");
-                 sscanf(satirTut,"%d",&head->plaka);
-                 printf("%d,", head -> plaka);
-                 fprintf(yaz,"%d,", head -> plaka);
-                 satirTut = strtok(NULL,",");
-                 sscanf(satirTut,"%s",&head->isim);
-                 printf("%s,", head -> isim);
-                 fprintf(yaz,"%s,", head -> isim);
-                 satirTut = strtok(NULL,",");
-                 sscanf(satirTut,"%s",&head->bolge);
-                 printf("%s,", head -> bolge);
-                 fprintf(yaz,"%s,", head -> bolge);
-                 head->komsuSayisi=virgul-2;
-                 fprintf(yaz,"%d\n",head->komsuSayisi);
-                 printf("%d\n",head->komsuSayisi);
-                     fprintf(yaz," |     /|\\\n");
-                     fprintf(yaz,"\\|/     |\n");
-                 virgul=0;
-                 head -> next=NULL;
+                satirTut = strtok(satir,",");
+                sscanf(satirTut,"%d",&head->plaka);
+                satirTut = strtok(NULL,",");
+                sscanf(satirTut,"%s",&head->isim);
+                satirTut = strtok(NULL,",");
+                sscanf(satirTut,"%s",&head->bolge);
+                head->komsuSayisi=virgul-2;
+                virgul=0;
+                head -> next=NULL;
             }
             else
             {
                 struct liste *current = head;
-            struct liste *temp = malloc(sizeof(liste));
+                struct liste *temp = malloc(sizeof(liste));
 
                 while(current -> next != NULL)
                 {
                     current = current -> next;
                 }
 
-                 if(temp->next !=NULL)
-                 {
-                     satirOku=fgets(satir,90,dosya);
-                     for(i=0; i<strlen(satir); i++)
-                     {
-                         if(satir[i]==',')
-                         {
-                             virgul++;
-                         }
-                     }
+                if(temp->next !=NULL)
+                {
+                    satirOku=fgets(satir,90,dosya);
+                    for(i=0; i<strlen(satir); i++)
+                    {
+                        if(satir[i]==',')
+                        {
+                            virgul++;
+                        }
+                    }
 
-                     yaz=fopen("OUTPUT.txt","a+");
-                     satirTut = strtok(satir,",");
-                     sscanf(satirTut,"%d",&temp->plaka);
-                     printf("%d,", temp -> plaka);
-                     fprintf(yaz,"%d,", temp -> plaka);
-                     satirTut = strtok(NULL,",");
-                     sscanf(satirTut,"%s",&temp->isim);
-                     printf("%s,", temp -> isim);
-                     fprintf(yaz,"%s,", temp -> isim);
-                     satirTut = strtok(NULL,",");
-                     sscanf(satirTut,"%s",&temp->bolge);
-                     printf("%s,", temp -> bolge);
-                     fprintf(yaz,"%s,", temp -> bolge);
-                     temp->komsuSayisi=virgul-2;
-
-                     printf("%d\n",temp->komsuSayisi);
-                     fprintf(yaz,"%d\n",temp->komsuSayisi);
-                     fprintf(yaz," |     /|\\\n");
-                     fprintf(yaz,"\\|/     |\n");
-                     virgul=0;
-
-
-                 }
-                 current -> next = temp;
-                 temp -> next = NULL;
-
-
-
-
+                    yaz=fopen("OUTPUT.txt","a+");
+                    satirTut = strtok(satir,",");
+                    sscanf(satirTut,"%d",&temp->plaka);
+                    satirTut = strtok(NULL,",");
+                    sscanf(satirTut,"%s",&temp->isim);
+                    satirTut = strtok(NULL,",");
+                    sscanf(satirTut,"%s",&temp->bolge);
+                    temp->komsuSayisi=virgul-2;
+                    virgul=0;
+                }
+                current -> next = temp;
+                temp -> next = NULL;
 
             }
         }
@@ -135,22 +101,21 @@ liste *listeOku(liste *head)
     }
     return head;
 }
-void printList(liste *head)
+liste *printList(liste *head)
 {
+
     liste *current = head;
     while (current != NULL)
     {
-       printf("%d,",current->plaka);
-       printf("%s,",current->isim);
-       printf("%s,",current->bolge);
-       printf("%d\n",current->komsuSayisi);
+        printf("%d,",current->plaka);
+        printf("%s,",current->isim);
+        printf("%s,",current->bolge);
+        printf("%d\n",current->komsuSayisi);
+        printf(" |     /|\\\n");
+        printf("\\|/     |\n");
+
         current = current -> next;
     }
-
-
-
-
-
 
     return;
 }
@@ -158,9 +123,10 @@ void printList(liste *head)
 int main()
 {
 
-    liste *A = NULL;
-    A=listeOku(A);
+    liste *A = listeOku(A);
+
     printList(A);
+
 
 
     return 0;
